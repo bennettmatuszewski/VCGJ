6090e596c8a032b8b38c0786706c51144e1f8350
Bennett <panzgfx@gmail.com>
2025-03-18 15:12:09 -0400

    imported necessary packages before the jam starts!

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UIElements;
+
+public class Test : MonoBehaviour
+{
+    public Image sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
